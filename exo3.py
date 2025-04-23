14982c0d92a8b247c48e70dd9b88cad748c93ea9
robinZafrani <robin.zafrani@gmail.com>
2025-04-23 10:51:45 +0200

    exo3

---
+++ exo3.py
@@ -0,0 +1,9 @@
+def plus_grand_nombre(liste):
+    max_nombre = liste[0]
+    for i in liste:
+        if i > max_nombre:
+            max_nombre = i
+    return max_nombre
+
+print(plus_grand_nombre([3, 57, 23, 56])) # Affiche 58
+
